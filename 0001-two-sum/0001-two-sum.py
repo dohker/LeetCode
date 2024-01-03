8673afc333b2e9f9a637058cb5005bf10124a934
Do-Hyung Kwon <dohk325@gmail.com>
2024-01-03 11:50:05 +0900

    Time: 3971 ms (8.22%), Space: 18.1 MB (17.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        output = []
         for i in range(len(nums)):
-            i_val = nums[i]
-            for j in range(1, len(nums)):
-                j_val = nums[j]
-                if i == j:
-                    break
-                if i_val + j_val == target:
-                    output.append(i)
-                    output.append(j)
-                    return output
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
