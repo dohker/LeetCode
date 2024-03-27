55701e8c511840b78613a3d6dc7498447165c75d
Do-Hyung Kwon <dohk325@gmail.com>
2024-03-27 22:39:44 +0900

    Time: 731 ms (72.03%), Space: 27.3 MB (90.56%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,11 +3,15 @@ class Solution:
         if len(prices) <= 1:
             return 0
         
-        min_price = prices[0]  # 초기 최소 가격은 첫 번째 가격으로 설정
+        min_price = prices[0]  # 현재까지의 최소 가격을 저장할 변수
         max_profit = 0  # 최대 이익 초기값은 0으로 설정
         
         for price in prices[1:]:
-            min_price = min(min_price, price)  # 현재 가격과 최소 가격 중 작은 값을 선택하여 업데이트
-            max_profit = max(max_profit, price - min_price)  # 현재 가격에서 최소 가격을 뺀 값과 최대 이익 중 큰 값을 선택하여 업데이트
+            if price > min_price:
+                # 현재 가격이 최소 가격보다 큰 경우
+                max_profit = max(max_profit, price - min_price)  # 최대 이익 업데이트
+            else:
+                # 현재 가격이 최소 가격보다 작은 경우
+                min_price = price  # 최소 가격 업데이트
         
         return max_profit
