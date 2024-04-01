fdb5dd4d84eeb9c48fa9bf48d23f317bf1ae907a
Do-Hyung Kwon <dohk325@gmail.com>
2024-04-01 12:25:42 +0900

    Time: 376 ms (33.80%), Space: 17.8 MB (87.73%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -20,13 +20,31 @@
             
 #         return False
 
+
+
+
+
+# class Solution:
+#     def canJump(self, nums: List[int]) -> bool:
+#         max_reachable = 0  # 현재까지 도달할 수 있는 최대 인덱스
+#         for i in range(len(nums)):
+#             if i > max_reachable:
+#                 return False  # 현재 위치에 도달할 수 없으면 False 반환
+#             max_reachable = max(max_reachable, i + nums[i])  # 최대 도달 가능 거리 갱신
+#             print("max_reachable: {}".format(max_reachable))
+#             if max_reachable >= len(nums) - 1:
+#                 return True  # 마지막 인덱스에 도달 가능하면 True 반환
+#         return False
+
+
+
+
+
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         max_reachable = 0  # 현재까지 도달할 수 있는 최대 인덱스
-        for i in range(len(nums)):
+        for i, jump in enumerate(nums):
             if i > max_reachable:
                 return False  # 현재 위치에 도달할 수 없으면 False 반환
-            max_reachable = max(max_reachable, i + nums[i])  # 최대 도달 가능 거리 갱신
-            if max_reachable >= len(nums) - 1:
-                return True  # 마지막 인덱스에 도달 가능하면 True 반환
-        return False
+            max_reachable = max(max_reachable, i + jump)  # 최대 도달 가능 거리 갱신
+        return True
