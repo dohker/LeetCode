250336736a1999a9c6783043c86bb0810f5fdd7f
Do-Hyung Kwon <dohk325@gmail.com>
2024-07-18 20:18:10 +0900

    Time: 44 ms (33.02%), Space: 16.7 MB (42.97%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,17 +1,13 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        def backtrack(start, memo):
-            if start == len(s):
-                return True
-            if memo[start] is not None:
-                return memo[start]
-            for end in range(start + 1, len(s) + 1):
-                if s[start:end] in wordDictSet and backtrack(end, memo):
-                    memo[start] = True
-                    return True
-            memo[start] = False
-            return False
-        
         wordDictSet = set(wordDict)
-        memo = [None] * len(s)
-        return backtrack(0, memo)
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDictSet:
+                    dp[i] = True
+                    break
+
+        return dp[len(s)]
