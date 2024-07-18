93ad62e034c8b8c04bfcafebd14494dd3a0d81d0
Do-Hyung Kwon <dohk325@gmail.com>
2024-07-18 22:43:06 +0900

    Time: 40 ms (18.67%), Space: 16.4 MB (52.01%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        tabular = []
-        tabular.append(1) # n = 0
-        tabular.append(1) # n = 1
+        tabular = [1, 1] # n = 0, 1
         
         for i in range(2, n+1):
-            sub_result = tabular[i-1] + tabular[i-2]
-            tabular.append(sub_result)
+            tabular.append(tabular[i-1] + tabular[i-2])
         
         return tabular[-1]
