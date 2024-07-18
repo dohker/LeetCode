d9814054dc7a01f7395c5f9a7cb75d1bd7c6c492
Do-Hyung Kwon <dohk325@gmail.com>
2024-07-18 22:11:53 +0900

    Time: 33 ms (68.17%), Space: 16.4 MB (52.01%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,16 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # n = 5
         memo = {}
+        def dp(n, memo):
+            if n == 1 or n == 0:
+                return 1
 
-        if n == 1 or n == 0:
-            return 1
+            if n in memo.keys():
+                return memo[n]
 
-        if n in memo.keys():
-            return memo[n]
-        else:
-            sub_result = self.climbStairs(n-1) + self.climbStairs(n-2)
-            memo[n] = sub_result
-            return memo[n]
+            else:
+                sub_result = dp(n-1, memo) + dp(n-2, memo)
+                memo[n] = sub_result
+                return memo[n]
+
+        return dp(n, memo)
