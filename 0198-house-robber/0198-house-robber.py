6cee0be9d7784f1d01b43aacc2a5ccd4fddbce2a
Do-Hyung Kwon <dohk325@gmail.com>
2024-07-25 18:42:58 +0900

    Time: 40 ms (26.79%), Space: 16.6 MB (23.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -6,9 +6,9 @@ class Solution:
             if state == 0:
                 return nums[state]
             if state == 1:
-                return max(nums[state-1], nums[state])
-            if state not in memo.keys():
-                memo[state] = max(dp(state-1), dp(state-2)+nums[state])
-            return memo[state]
+                return max(nums[state], nums[state-1])
 
+            if state not in memo.keys():
+                memo[state] = max(dp(state-1), dp(state-2) + nums[state])
+            return memo[state]
         return dp(len(nums)-1)
