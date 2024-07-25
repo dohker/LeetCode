62107aff1f72a79c4743f0611eda06d3cb41e4eb
Do-Hyung Kwon <dohk325@gmail.com>
2024-07-25 18:38:22 +0900

    Time: 44 ms (8.26%), Space: 16.5 MB (62.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,17 +1,37 @@
-from typing import List
+'''
+state: 현재 집의 위치
+
+상태 전이:
+    f(3) = max(1 + f(2) or 1 + f(2))
+    f(2) = 3 + f(1) or 3 + f(0)
+    f(1) = 2
+    f(0) = 1
+
+base case:
+    f(1) = 2
+    f(0) = 1
+
+dp(state) = max(nums[state] + dp(state-1), nums[state] + dp(state-2))
+
+if state in memo.keys():
+    return memo[state]
+else:
+    memo[state] = max(nums[state] + dp(state-1), nums[state] + dp(state-2))
+    return memo[state]
+'''
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        if len(nums) == 1:
-            return nums[0]
-        
-        prev1, prev2 = 0, 0
-        
-        for num in nums:
-            temp = prev1
-            prev1 = max(prev1, prev2 + num)
-            prev2 = temp
-        
-        return prev1
+        def dp(state: int):
+            if state == 0:
+                return nums[0]
+            if state == 1:
+                return max(nums[0], nums[1])
+
+            if state not in memo.keys():
+                memo[state] = max(dp(state-1), dp(state-2)+nums[state])
+
+            return memo[state]
+
+        memo = {}
+        return dp(len(nums)-1)
