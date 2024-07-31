4d290500a296c4ca047c3458d5e9d89288aa44a2
Do-Hyung Kwon <dohk325@gmail.com>
2024-07-31 11:19:52 +0900

    Time: 33 ms (74.07%), Space: 16.5 MB (86.32%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -3,40 +3,21 @@
     - 첫 원소는 값 추가
     - 두 번째 원소 부터는 
         - open bracket이면 추가
-        - close bracket이면 peek으로 스택 값 확인 후 valid 체크
+        - close bracket이면 스택 마지막 값 확인 후 valid 체크
     - 스택에 남아 있는 원소가 없으면 True
 '''
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = [s[0]]
+        stack = []
+        bracket_map = {')': '(', ']': '[', '}': '{'}
 
-        for i in range(1, len(s)):
-            if s[i] in (')', ']', '}'):
-                if len(stack) == 0:
+        for char in s:
+            if char in bracket_map:
+                top_element = stack.pop() if stack else '#'
+                if bracket_map[char] != top_element:
                     return False
-                match stack[-1]:
-                    case '(':
-                        if s[i] == ')':
-                            stack.pop()
-                        else:
-                            stack.append(s[i])
-                    case '[':
-                        if s[i] == ']':
-                            stack.pop()
-                        else:
-                            stack.append(s[i])
-                    case '{':
-                        if s[i] == '}':
-                            stack.pop()
-                        else:
-                            stack.append(s[i])
-                    case _:
-                        stack.append(s[i])
             else:
-                stack.append(s[i])
-        print(f'stack:{stack}')
-        if len(stack) == 0:
-            return True
-        return False
-        
+                stack.append(char)
+
+        return not stack
