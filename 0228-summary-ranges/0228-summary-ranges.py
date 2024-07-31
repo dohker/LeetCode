45a52799fe82782153cd7c359a4e9b2676d46db2
Do-Hyung Kwon <dohk325@gmail.com>
2024-07-31 10:46:06 +0900

    Time: 33 ms (69.59%), Space: 16.6 MB (23.16%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -13,28 +13,20 @@ class Solution:
 
         prev = nums[0]
         output = [str(prev)+"->"]
-        # print(f'prev: {prev}')
-        # print(f"output: {output}")
 
         for i in range(1,len(nums)):
-            print(f'nums[{i}]: {nums[i]}')
             if (nums[i]-prev) != 1:
                 output[-1] = output[-1]+str(prev)
                 output.append(str(nums[i])+"->")
-            # print(f'output: {output}')
             if i == len(nums)-1 and (nums[i]-prev) == 1:
                 output[-1] = output[-1]+str(nums[i])
             prev = nums[i]
         
-            
         if output[-1].endswith('->'):
             output[-1] = output[-1].replace('->', '')
         for idx, value in enumerate(output):
-            print(f'output: {output}')
             temp = value.split('->')
-            print(f'temp: {temp}')
             if len(temp) >= 2 and temp[0] == temp[1]:
-                print(f'temp: {temp}')
                 output[idx] = temp[0]
-        # print(f'output: {output}')
+
         return output
