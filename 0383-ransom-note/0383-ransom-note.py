3791654ac47a4c8525983295a5454a65b1ea6318
Do-Hyung Kwon <dohk325@gmail.com>
2024-07-31 10:06:25 +0900

    Time: 50 ms (77.68%), Space: 16.6 MB (58.56%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -24,6 +24,6 @@ class Solution:
             if result == 0:
                 return True
         
-        if result == 0:
-            return True
+        # if result == 0:
+        #     return True
         return False
